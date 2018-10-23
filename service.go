46d3b3e7b9d77ca83c814a57483705e971b8f8e9
Korinoken <nikolajs.pelniks@gmail.com>
2018-10-23 18:16:42 +0300

    Documentation fixes

--- service.go
+++ service.go
@@ -48,19 +48,19 @@ func (c *LeaderboardService) Run(cfg Config) error {
 	ws := new(restful.WebService)
 	ws.
 		Path("/leaderboard").
-		Doc("DocTest").
-		Consumes(restful.MIME_JSON, restful.MIME_XML).
 		Produces(restful.MIME_JSON, restful.MIME_XML)
 
-	ws.Route(ws.POST("{tournaments}").To(model.AddTournamentAndResults).
+	ws.Route(ws.POST("tournaments").To(model.AddTournamentAndResults).
 		Doc("add tournament and participants by url without domain").
 		Operation("AddTournamentAndResults").
 		Reads(api.AddTournamentRequest{}))
-	ws.Route(ws.DELETE("{tournaments}/{tournament-url}").To(model.DeleteTournament).
+	ws.Route(ws.DELETE("tournaments/{tournament-url}").To(model.DeleteTournament).
 		Doc("delete tournament and results by url without domain").
+		Param(ws.PathParameter("tournament-url", "Name of the tournament as is in Chalonge url").DataType("string")).
 		Operation("DeleteTournament"))
-	ws.Route(ws.GET("{tournaments}/{tournament-url}").To(model.GetTournamentData).
+	ws.Route(ws.GET("tournaments/{tournament-url}").To(model.GetTournamentData).
 		Doc("Get details for a tournament by url without domain").
+		Param(ws.PathParameter("tournament-url", "Name of the tournament as is in Chalonge url").DataType("string")).
 		Operation("GetTournamentData").
 		Writes(api.Tournament{}))
 	ws.Route(ws.GET("scoreboard").To(model.GetScore).
@@ -69,6 +69,7 @@ func (c *LeaderboardService) Run(cfg Config) error {
 		Writes([]api.ParticipantResults{}))
 	ws.Route(ws.GET("participants/{participant-name}/results").To(model.GetParticipantData).
 		Doc("Get details for participant").
+		Param(ws.PathParameter("participant-name", "Challonge registration name of the participant").DataType("string")).
 		Operation("GetScore"))
 	ws.Route(ws.POST("participants").To(model.CreateParticipantDetails).
 		Doc("Add details to participant").
@@ -78,19 +79,16 @@ func (c *LeaderboardService) Run(cfg Config) error {
 		Doc("Edit participant detials").
 		Operation("CreateParticipantDetails").
 		Reads(api.ParticipantDetails{}))
-	ws.Route(ws.GET("participants/{participant-name}/details").To(model.GetParticipantDetails).
+	ws.Route(ws.GET("participants/{participant-name}").To(model.GetParticipantDetails).
 		Doc("Get details for participant").
+		Param(ws.PathParameter("participant-name", "Challonge registration name of the participant").DataType("string")).
 		Operation("GetParticipantDetails").
 		Writes(api.ParticipantDetails{}))
 	restful.Add(ws)
 
-	//TODO: fix swagger and add annotations
 	svcConfig := swagger.Config{
-		WebServices:     restful.RegisteredWebServices(),
-		WebServicesUrl:  cfg.SvcHost,
-		ApiPath:         "/apidocs.json",
-		SwaggerPath:     "/apidocs/",
-		SwaggerFilePath: ".\\dist"}
+		WebServices: restful.RegisteredWebServices(),
+		ApiPath:     "/leaderboard/docs"}
 	swagger.InstallSwaggerService(svcConfig)
 
 	log.Printf("start listening on localhost %v", cfg.SvcHost)
